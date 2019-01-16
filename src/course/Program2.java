eb7ab10ae6c9785113b78ded736dce8b643242b3
alesbernardes <alesbernardes@gmail.com>
2019-01-16 12:00:37 -0200

    Projeto criado

---
+++ src/course/Program2.java
@@ -0,0 +1,16 @@
+package course;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String original ="abcde FGHIJ ABC abc DEFG";
+		
+		String s01 = original.replace("a","x");
+		
+		int h = (10 > 4) ? 50:20; 
+		System.out.println(s01);
+		System.out.println(h);
+	}
+
+}
